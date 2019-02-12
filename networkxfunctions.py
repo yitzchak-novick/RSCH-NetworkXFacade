38ed9cd8f4179c6144bc62d4c1532dbe25d4b63a
ynovick-Touro <yitzchak.novick@touro.edu>
2019-02-12 14:17:41 -0500

    Create networkxfunctions.py

---
+++ networkxfunctions.py
@@ -0,0 +1,24 @@
+def get_graph_from_file(file_path):
+    edges = []
+    with open(file_path, 'r') as f:
+        for line in f:
+            if not line[0] == '%' and not line[1] == '#':
+                parts = line.split()
+                edges.append((parts[0], parts[1]))
+    graph = nx.Graph()
+    graph.add_edges_from(edges)
+    return graph
+
+def get_node_dict(graph, n_id):
+    n_dict =  {
+            'id': n_id,
+            'degree': graph.degree(n_id),
+            'fi': sum([graph.degree(n1) for n1 in graph.neighbors(n_id)])/graph.degree(n_id)**2
+        }
+    return n_dict
+
+def get_node_dicts(graph):
+    return [get_node_dict(graph, n) for n in graph.nodes()]
+
+def assort(graph):
+    return nx.degree_assortativity_coefficient(graph)
